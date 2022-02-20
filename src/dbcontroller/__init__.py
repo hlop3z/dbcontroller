0db9140e2f9792688893d2a34180f3bf772931e4
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-02-19 22:25:57 -0600

    Objects util

--- src/dbcontroller/__init__.py
+++ src/dbcontroller/__init__.py
@@ -3,3 +3,4 @@ Controller for <Mongo & SQL>
 """
 
 from .manager import Mongo, Sql
+from .manager.utils import Objects
--- src/dbcontroller/manager/utils/__init__.py
+++ src/dbcontroller/manager/utils/__init__.py
@@ -6,3 +6,17 @@ from .object_id import mongo_id_decode, sql_id_decode
 from .pagination import pagination
 from .response import Response
 from .row_handler import to_obj
+
+
+class Objects:
+    """Convert <SQL or Mongo> output to types.SimpleNamespace => Object(s)."""
+
+    @staticmethod
+    def mongo(items):
+        """Convert Mongo to SimpleNamespace"""
+        return to_obj(items)
+
+    @staticmethod
+    def sql(items):
+        """Convert SQLAlchemy to SimpleNamespace"""
+        return to_obj(items, sql=True)
